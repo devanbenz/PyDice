7de3426ed1535ec562fa13175e0d8dc2f52d9977
devanbenz <devandbenz@gmail.com>
2021-03-02 21:05:34 -0600

    Started creating a rudimentary inventory file and json itemlist to working with still a work in progress :/

--- inventory.py
+++ inventory.py
@@ -1,5 +1,18 @@
+import json as j
+
+with open("itemList.json","r") as read_file:
+    json.
+
 ## TODO: CREATE CLASS INVENTORY 
 
+## Class Inventory will initialize with basic user gear - weapon fist, gear tattered clothing
+class Inventory:
+    def __init__(self, inv):
+        inv = [100,2]
+
+    # Getter for retrieving inventory data 
+    def getInv(self, inv):
+        return self.inv
 ## TODO: SET CLASS INITIALISATION THAT GIVE CHARACTER BASIC INVENTORY
 
 ## TODO: METHODS FOR INVENTORY CLASS - GET INVENTORY, ADD TO INVENTORY, REMOVE FROM INVENTORY 
