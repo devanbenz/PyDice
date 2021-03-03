55c886f4fb0bfbe44f6d8d7a0a8096868a4dea66
WeblWabl <devandbenz@gmail.com>
2021-03-02 21:08:58 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,18 +1,17 @@
+## TODO: Need to implement unmarshalling json to variables
 import json as j
+import os
 
+## baby steps! Using realpython to relearn a ton of this stuff ;P
 with open("itemList.json","r") as read_file:
-    json.
-
-## TODO: CREATE CLASS INVENTORY 
 
 ## Class Inventory will initialize with basic user gear - weapon fist, gear tattered clothing
 class Inventory:
     def __init__(self, inv):
         inv = [100,2]
 
+## TODO: METHODS FOR INVENTORY CLASS - GET INVENTORY, ADD TO INVENTORY, REMOVE FROM INVENTORY 
+
     # Getter for retrieving inventory data 
     def getInv(self, inv):
         return self.inv
-## TODO: SET CLASS INITIALISATION THAT GIVE CHARACTER BASIC INVENTORY
-
-## TODO: METHODS FOR INVENTORY CLASS - GET INVENTORY, ADD TO INVENTORY, REMOVE FROM INVENTORY 
